da58f4db5ec810b04b6e21d555058390dd9dac1b
wen <yzwen509@gmail.com>
2016-08-11 18:22:25 +0800

    dd

---
+++ java/AssicHexStringConvert.java
@@ -0,0 +1,45 @@
+public class AssicHexStringConvert {
+
+	/**
+	 * 十六进制字符转Assic码字符
+	 * @param hex
+	 * @return
+	 */
+	public static String convertHexToString(String hex){
+
+		StringBuilder sb = new StringBuilder();
+		StringBuilder temp = new StringBuilder();
+
+		//49204c6f7665204a617661 split into two characters 49, 20, 4c...
+		for( int i=0; i<hex.length()-1; i+=2 ){
+
+			//grab the hex in pairs
+			String output = hex.substring(i, (i + 2));
+			//convert hex to decimal
+			int decimal = Integer.parseInt(output, 16);
+			//convert the decimal to character
+			sb.append((char)decimal);
+
+			temp.append(decimal);
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * Assic码转十六进制
+	 * @param str
+	 * @return
+	 */
+	public static String convertStringToHex(String str){
+
+		char[] chars = str.toCharArray();
+
+		StringBuffer hex = new StringBuffer();
+		for(int i = 0; i < chars.length; i++){
+			hex.append(Integer.toHexString((int)chars[i]));
+		}
+
+		return hex.toString();
+	}
+}
